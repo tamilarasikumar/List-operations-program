94b1d461323cefb624cc80abbd5c0707a0143e0d
tamilarasikumar <tamilarasiifet@gmail.com>
2021-07-20 20:20:43 +0530

    movies completed

---
+++ movies.py
@@ -0,0 +1,10 @@
+movies_list=["master","sultan","hero","karnan",96]
+movies_no=[1,2,3,4,5]
+del movies_list[1]  #del index
+movies_list.remove("hero") #del specific name
+#movies_list.clear() #del all list
+movies_list.pop(1) #del specific list
+print(len(movies_list)) #length of list
+print(movies_list+movies_no) #concatenate list
+print(movies_list*2) #reputation
+print(movies_list)
